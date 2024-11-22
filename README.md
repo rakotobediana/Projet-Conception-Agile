037420b50937565068de865fba62bc147b9bdd67
rakotobediana <rkt.diana@gmail.com>
2024-11-22 16:45:49 +0100

    insertion des cartes dans dossier images

--- README.md
+++ README.md
@@ -1,5 +1,4 @@
 # Projet-Conception-Agile
-
 Binôme: 
 NGUYEN Do Minh Trang - 5221045 
 RAKOTOBE Diana - 5203849
--- views.py
+++ views.py
@@ -1,6 +1,7 @@
 from flask import render_template, request, redirect, url_for
 from flask import Blueprint
 
+
 views = Blueprint('views', __name__, template_folder='templates')
 
 # Route principale pour la page d'accueil
