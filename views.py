dfe7d9a8d630f36da9e816a8e8170b12e6f07ece
rakotobediana <rkt.diana@gmail.com>
2024-12-20 13:41:03 +0100

    blacklog ...

--- views.py
+++ views.py
@@ -1,6 +1,7 @@
 from flask import render_template, request, redirect, url_for
 from flask import Blueprint
 from models.joueur import Joueur
+import json
 
 views = Blueprint('views', __name__, template_folder='../templates')
 
@@ -42,10 +43,40 @@ def config_joueur():
 
 
 # Route pour débuter une partie de poker 
-@views.route('/listeJoueur/jeu', methods=['POST'])
+@views.route('/listeJoueur/jeu',methods=['GET', 'POST'])
 def jeu():
 
+
+
+
     global nb_player
+
+    # Charger le fichier JSON contenant les éléments du backlog
+    with open('backlog.json', 'r') as f:
+        data = json.load(f)
+
+    backlog = data['backlog']
+    
+    # Récupérer le numéro de la question à partir de l'URL, sinon commencer à 1
+    numero = 1
+
+    # Chercher la question correspondant au numéro
+    question = next((q for q in backlog if q["numero"] == numero), None)
+
+    # Si la question existe, récupérer l'index pour savoir s'il y a une question suivante
+    if question:
+        # Nombre total de questions
+        nb_items = len(backlog)
+        next_question = numero + 1 if numero < nb_items else None
+    else:
+        question = {"numero": "Erreur", "titre": "Question non trouvée"}
+        next_question = None
+
+
+
+
+
+
     nb_player = int(nb_player)
     joueurs = []
     if request.method == 'POST':
@@ -59,5 +90,5 @@ def jeu():
     cartes = ["cartes_0.svg", "cartes_1.svg", "cartes_2.svg", "cartes_3.svg", 
               "cartes_5.svg", "cartes_8.svg", "cartes_13.svg", "cartes_20.svg", 
               "cartes_40.svg", "cartes_100.svg", "cartes_cafe.svg", "cartes_interro.svg"] 
-    return render_template('jeu.html', cartes=cartes , nb_player=nb_player , joueurs=joueurs)
+    return render_template('jeu.html', cartes=cartes , nb_player=nb_player , joueurs=joueurs , question=question, numero=numero, next_question=next_question)
 
